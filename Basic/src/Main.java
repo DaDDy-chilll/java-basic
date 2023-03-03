3cdceaa559d2330ae540ab1f4639eb0cc95f65ff
DaDDy <blacklistuser420@gmail.com>
2023-03-03 18:19:00 +0630

    data types

--- Basic/src/Main.java
+++ Basic/src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String[] bm) {
-        int x = 10;
-        int y = 20;
-        int z = x + y;
-        boolean check = x > y;
-        String name = "DaDDy";
-        System.out.println("Z Variable value " + z + check + name);
+//        int x = 10;
+//        int y = 20;
+//        int z = x + y;
+//        boolean check = x > y;
+//        String name = "DaDDy";
+
+//        int x = 10; // initialization
+//        int t; //declare
+//        t=20;
+//        t=50;//reassign
+//        x=100;
+
+        byte enroll_studens = 127;
+
+        float x = 1.12312345f;
+        double d = 1.12312312312312312399;
+        boolean b = 45 < 44;
+        char c = 'c';
+
+        System.out.println(" Variable value " + x  );
+        System.out.println(" Double value " + d  );
     }
 }
