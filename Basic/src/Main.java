b0c1fd0ca66cfcb3a42de28173d85fc9ec3ddb7a
DaDDy <blacklistuser420@gmail.com>
2023-03-04 19:46:46 +0630

    opertor

--- Basic/src/Main.java
+++ Basic/src/Main.java
@@ -12,14 +12,53 @@ public class Main {
 //        t=50;//reassign
 //        x=100;
 
-        byte enroll_studens = 127;
+//        byte enroll_studens = 127;
+//
+//        float x = 1.12312345f;
+//        double d = 1.12312312312312312399;
+//        boolean b = 45 < 44;
+//        char c = 'c';
 
-        float x = 1.12312345f;
-        double d = 1.12312312312312312399;
-        boolean b = 45 < 44;
-        char c = 'c';
+//        int num_of_student = 60;
+//        int absenced_student = 20;
+//        int presented_student = num_of_student - absenced_student;
+//        System.out.println("Current presented student count is " + presented_student);
 
-        System.out.println(" Variable value " + x  );
-        System.out.println(" Double value " + d  );
+//        int episode = 20;
+//        int series = 5;
+//        int total = episode * series;
+//        System.out.println("Total epsiode count is " + total);
+
+//        int people = 12098;
+//        int car = 150;
+//        int count_people_in_a_car = people / car;
+//        System.out.println("Each car will hold" + count_people_in_a_car + " people.");
+
+//            int count = 10;
+//            count++;
+//            count--;
+//        System.out.println(count);
+
+//        boolean bol = 200 >= 200;
+//        System.out.println(bol);
+
+//        int age = 20;
+//        int permit = 19;
+//
+//        boolean check =  age == 20 || permit > age;
+//        System.out.println(!check);
+
+        int x = 20;
+//        x += 20;
+//        x -=10;
+//        x *= 2;
+//        x /= 3;
+//        x %= 5;
+        x = x == 20 ? 30 : 50;
+        System.out.println(x);
     }
 }
+
+
+
+
